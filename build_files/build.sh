dec2eb53c54cc2a5d55952ae5c30dca689dec8ed
irunatbullets <murray@ik.me>
2026-03-19 12:52:06 +0100

    swap gnome to niri

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,27 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 -y remove                  \
+    gnome-shell                 \
+    gnome-session               \
+    mutter
+
+# setup niri
+dnf5 -y install                 \
+    niri                        \
+    foot                        \
+    mako                        \
+    fuzzel                      \
+    waybar                      \
+    swaybg                      \
+    swayidle                    \
+    swaylock                    \
+    xwayland-satellite          \
+    xdg-desktop-portal-gtk      \
+    xdg-desktop-portal-gnome    \
+    gnome-keyring               \
+    nautilus                    \
+    polkit-gnome
 
 # Use a COPR Example:
 #
@@ -22,3 +42,4 @@ dnf5 install -y tmux
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
+

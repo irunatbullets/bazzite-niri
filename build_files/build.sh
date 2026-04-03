3659b0aa28bd5f89065d2c3ac2741910d1d6737b
irunatbullets <murray@ik.me>
2026-04-03 13:29:12 +0200

    add tuigreet and gtklock

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,11 +9,33 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# remove kde plasma
+# Remove kde plasma
 dnf5 -y remove plasma-workspace plasma-* kde-* sddm
+
+# Remove existing bazzite pipewire stuff
+dnf5 versionlock delete \
+    pipewire \
+    pipewire-alsa \
+    pipewire-gstreamer \
+    pipewire-jack-audio-connection-kit \
+    pipewire-jack-audio-connection-kit-libs \
+    pipewire-libs \
+    pipewire-plugin-libcamera \
+    pipewire-pulseaudio \
+    pipewire-utils \
+    wireplumber \
+    wireplumber-libs
+
+# Tidy up!
 dnf5 -y autoremove
 
-# setup niri "important software"
+# Install pipewire
+dnf5 -y install \
+    pipewire \
+    pipewire-libs \
+    pipewire-devel
+
+# Setup niri "important software"
 dnf5 -y install \
     gnome-keyring \
     xfce-polkit \
@@ -23,7 +45,7 @@ dnf5 -y install \
     xdg-desktop-portal-gtk \
     xwayland-satellite
 
-# setup niri
+# Setup niri
 dnf5 -y install \
     alacritty \
     fuzzel \
@@ -33,16 +55,37 @@ dnf5 -y install \
     swaylock \
     waybar
 
-# install desktop manager
-dnf5 -y install \
-    ly
-
-# install rust and dependencies
+# Install rust, cargo and various dependencies
 dnf5 -y install \
     rust \
     cargo \
     dbus-devel \
     pkgconf-pkg-config
+
+dnf5 -y install \
+    tuigreet
+
+# Build gtklock from source
+dnf5 -y install \
+    meson \
+    ninja \
+    gcc \
+    gtk3-devel \
+    pam-devel \
+    wayland-devel \
+    wayland-protocols-devel \
+    gobject-introspection-devel \
+    vala
+
+git clone https://github.com/jovanlanik/gtklock.git /tmp/gtklock
+cd /tmp/gtklock
+
+meson setup build --prefix=/usr --libdir=/usr/lib64
+ninja -C build
+ninja -C build install
+
+cd /
+rm -rf /tmp/gtklock
 
 # Use a COPR Example:
 #

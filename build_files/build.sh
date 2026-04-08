22a721d9ec6249cdaab09c41c4d9afb0e8eb580d
irunatbullets <murray@ik.me>
2026-04-08 13:29:29 +0200

    overhaul, switched to bazzite-gnome to simplify a lot of things

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,20 +9,9 @@ set -euxo pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# Remove kde plasma
-dnf5 -y remove plasma-workspace plasma-* kde-* sddm
-
-# Tidy up!
-dnf5 -y autoremove
-
-# Setup niri "important software"
+# Setup niri "important software" not already covered by gnome.
 dnf5 -y install \
-    gnome-keyring \
     mako \
-    xfce-polkit \
-    nautilus \
-    xdg-desktop-portal-gnome \
-    xdg-desktop-portal-gtk \
     xwayland-satellite
 
 # Setup niri
@@ -34,112 +23,17 @@ dnf5 -y install \
     swayidle \
     waybar
 
-# Install tuigreet
-dnf5 -y install \
-    greetd \
-    tuigreet
-
-START_NIRI="/usr/local/bin/start-niri"
-
-if [ -L /usr/local ] || [ -e /usr/local ] && [ ! -d /usr/local ]; then
-    rm -rf /usr/local
-fi
-
-install -d /usr/local/bin
-
-install -Dm755 /dev/stdin "$START_NIRI" <<'EOF'
-#!/usr/bin/env bash
-
-cd "$HOME" || exit 1
-
-exec niri-session
-EOF
-
-CONFIG_GREETD="/etc/greetd/config.toml"
-install -Dm644 /dev/stdin "$CONFIG_GREETD" <<EOF
-[terminal]
-vt = 1
-
-[default_session]
-command = "tuigreet --remember --cmd /usr/local/bin/start-niri"
-user = "greetd"
-EOF
-
-install -d -o greetd -g greetd -m 0755 /var/cache/tuigreet
-
-# Build gtklock from source
-dnf5 -y install \
-    meson \
-    ninja \
-    gcc \
-    gtk3-devel \
-    pam-devel \
-    wayland-devel \
-    wayland-protocols-devel \
-    gobject-introspection-devel \
-    vala
-
-git clone https://github.com/jovanlanik/gtklock.git /tmp/gtklock
-cd /tmp/gtklock
-
-meson setup build --prefix=/usr --libdir=/usr/lib64
-ninja -C build
-ninja -C build install
-
-cd /
-rm -rf /tmp/gtklock
-
 # Install a couple of tuis
 dnf5 -y install \
     htop \
     wiremix
 
-# Install rust, cargo and various dependencies
+dnf5 -y copr enable binarypie/hypercube
 dnf5 -y install \
-    rust \
-    cargo \
-    clang \
-    clang-devel \
-    dbus-devel \
-    pkgconf-pkg-config
+    bluetui \
+    wifitui
+dnf5 -y copr disable binarypie/hypercube
 
-export CARGO_HOME=/tmp/cargo
-export RUSTUP_HOME=/tmp/rustup
-export CARGO_INSTALL_ROOT=/usr
-
-cargo install wifitui
-cargo install bluetui
-
-install -Dm644 /dev/stdin /etc/polkit-1/rules.d/49-wifitui.rules <<'EOF'
-polkit.addRule(function(action, subject) {
-    // Allow all users in the 'wheel' group to scan Wi-Fi
-    if (action.id == "org.freedesktop.NetworkManager.wifi.scan" &&
-        subject.isInGroup("wheel")) {
-        return polkit.Result.YES;
-    }
-});
-EOF
-
-rm -rf /tmp/cargo /tmp/rustup
-
-# Rmove rust build dependencies (except pkgconf-pkg-config)
-dnf5 -y remove \
-    rust \
-    cargo \
-    clang \
-    clang-devel \
-    dbus-devel
-
-dnf5 -y remove \
-    meson \
-    ninja \
-    gcc \
-    gtk3-devel \
-    pam-devel \
-    wayland-devel \
-    wayland-protocols-devel \
-    gobject-introspection-devel \
-    vala
 
 # Use a COPR Example:
 #
@@ -149,10 +43,7 @@ dnf5 -y remove \
 # dnf5 -y copr disable ublue-os/staging
 
 #### Example for enabling a System Unit File
-
 systemctl enable podman.socket
-systemctl enable greetd
 systemctl --global add-wants niri.service mako.service
 systemctl --global add-wants niri.service swayidle.service
-systemctl --global add-wants niri.service xfce-polkit.service
 

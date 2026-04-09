3d7b54443a97882dfd60fd21957bd68ec1df05ba
irunatbullets <murray@ik.me>
2026-04-09 09:53:43 +0200

    switch to building xwayland-satellite during image creation

--- build_files/build.sh
+++ build_files/build.sh
@@ -3,6 +3,15 @@
 set -euxo pipefail
 
 ### Install packages
+
+# NOTE:
+# The following "important software" for niri are already installed with gnome
+# which comes with the bazzite-gnome base image:
+#
+# gnome-keyring
+# xdg-desktop-portal-gnome
+# xdg-desktop-portal-gtk
+
 dnf5 -y install         \
     alacritty           \
     fuzzel              \
@@ -13,22 +22,34 @@ dnf5 -y install         \
     swayidle            \
     waybar              \
     wiremix             \
-    xfce-polkit         \
-    xwayland-satellite
+    xfce-polkit
 
-### Build fresh copies of wifitui and bluetui
-dnf5 -y install rust cargo @development-tools dbus-devel
+### Build fresh copies of wifitui, bluetui, and xwayland-satellite
+dnf5 -y install rust cargo @development-tools dbus-devel xcb-util-cursor-devel clang git
+
 (
     export CARGO_HOME=/tmp/cargo
     export RUSTUP_HOME=/tmp/rustup
     export CARGO_INSTALL_ROOT=/usr
 
+    # Install wifitui and bluetui
     cargo install wifitui bluetui
+
+    # Build xwayland-satellite from specific commit
+    cd /tmp
+    git clone https://github.com/Supreeeme/xwayland-satellite.git
+    cd xwayland-satellite
+    git checkout a879e5e
+
+    cargo build --release
+
+    # Copy binary to same place as cargo install (/usr/bin)
+    install -Dm755 target/release/xwayland-satellite /usr/bin/xwayland-satellite
 )
 
-###  Clean up build artifacts and dev packages
-rm -rf /tmp/cargo /tmp/rustup
-dnf5 -y remove rust cargo @development-tools dbus-devel
+### Clean up build artifacts and dev packages
+rm -rf /tmp/cargo /tmp/rustup /tmp/xwayland-satellite
+dnf5 -y remove rust cargo @development-tools dbus-devel xcb-util-cursor-devel clang git
 
 ### Copr install
 dnf5 -y copr enable mochaa/gtklock
